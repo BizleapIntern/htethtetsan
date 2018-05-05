e26b1b3ecb262756d1c2d8885af1818adcda30ef
BizleapIntern <bizleapinternship@gmail.com>
2018-05-04 22:18:26 -0700

    Bizleap String

---
+++ src/com/bizleap/training/assignment/five/madeStringList.java
@@ -0,0 +1,8 @@
+package com.bizleap.training.assignment.five;
+
+import java.util.List;
+
+public interface madeStringList {
+    public List generateList(int n);
+    public void testGenerateList();
+}
